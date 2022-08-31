8695c0ef5c422924b6fd47b2f3feb183ed8681c4
Михаил Белодед <mbeloded200@gmail.com>
2022-08-31 15:37:40 +0300

    Bugs fixed.

--- OnlineTradePlatform/OnlineTradePlatform.Core/Entities/User.cs
+++ OnlineTradePlatform/OnlineTradePlatform.Core/Entities/User.cs
@@ -4,8 +4,8 @@
     {
         public string Name { get; set; }
 
-        string Email { get; set; }
+        public string Email { get; set; }
 
-        string PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
