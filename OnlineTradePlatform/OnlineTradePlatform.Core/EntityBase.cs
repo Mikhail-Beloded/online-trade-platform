53aee781025f1655facfc685fd71152fe1ee3fa2
Михаил Белодед <mbeloded200@gmail.com>
2022-08-31 13:27:39 +0300

    Core layer was created.

---
+++ OnlineTradePlatform/OnlineTradePlatform.Core/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineTradePlatform.Core
+{
+    public class EntityBase
+    {
+        [Key]
+
+        public int Id { get; set; }
+    }
+}
