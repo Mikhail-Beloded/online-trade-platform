2cfc10f8db83ba6e8f565123a96e161e8d3c9a4a
Михаил Белодед <mbeloded200@gmail.com>
2022-09-02 22:17:21 +0300

    Entity framework context was developed. Also were created Fluent API configurations.

---
+++ OnlineTradePlatform/OnlineTradePlatform.Infrastructure/FluentAPI/AdConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using OnlineTradePlatform.Core.Entities;
+
+namespace OnlineTradePlatform.Infrastructure.FluentAPI
+{
+    public class AdConfiguration : IEntityTypeConfiguration<Ad>
+    {
+        public void Configure(EntityTypeBuilder<Ad> builder)
+        {
+            builder.HasOne<User>(x => x.User)
+                   .WithMany(y => y.Ads);
+        }
+    }
+}
