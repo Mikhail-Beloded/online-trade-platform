924f6455b6e8eb2aa9fbf2818e772cca9426dc6a
Михаил Белодед <mbeloded200@gmail.com>
2022-09-05 02:38:25 +0300

    Infrastructure layer was developed.

---
+++ OnlineTradePlatform/OnlineTradePlatform.Infrastructure/DepdendencyInjection.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OnlineTradePlatform.Application.IServices;
+using OnlineTradePlatform.Infrastructure.Services;
+using OnlineTradePlatform.Infrastructure.Context;
+using OnlineTradePlatform.Application.IRepositories;
+using OnlineTradePlatform.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace OnlineTradePlatform.Infrastructure
+{
+    public class DepdendencyInjection
+    {
+        public static IServiceCollection ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IAdService, AdService>();
+            services.AddScoped<IUsersService, UsersService>();
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureInfrastruction(IServiceCollection services)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("DBConnectionString");
+
+            services.AddDbContext<EFContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+            return services;
+        }
+    }
+}
